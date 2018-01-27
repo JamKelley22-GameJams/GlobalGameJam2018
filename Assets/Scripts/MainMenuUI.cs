05544b2dc64472194e7aa5db464e39b4320e1791
Devi Acharya <dacharya64@gmail.com>
2018-01-27 14:33:07 -0500

    Added main menu scene and UI assets

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour {
+
+    // Use this for initialization
+    public Button QuitButton;
+
+    void Start (){
+        QuitButton.onClick.AddListener(QuitApplication);
+    }
+
+    void QuitApplication() {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+}
+
