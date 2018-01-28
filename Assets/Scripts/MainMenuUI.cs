00b615e9acdc8c336fd5862dc94156a1acbe08dd
Devi Acharya <dacharya64@gmail.com>
2018-01-27 21:25:21 -0500

    Menus now link to one another

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
 
     // Use this for initialization
+    public Button PlayButton;
+    public Button CreditsButton;
     public Button QuitButton;
 
     void Start (){
+        PlayButton.onClick.AddListener(GoToLevelSelect);
+        CreditsButton.onClick.AddListener(GoToCredits);
         QuitButton.onClick.AddListener(QuitApplication);
     }
+
+    void GoToLevelSelect() {
+        SceneManager.LoadScene("LevelLoader", LoadSceneMode.Single);
+    }
+
+    void GoToCredits() {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
 
     void QuitApplication() {
         Application.Quit();
